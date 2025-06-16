f4865ef0c61134c0fba1179453dc9c1bb6a614c3
Nardone Giampietro <nashj6@live.it>
2025-06-16 13:16:07 +0200

    Update File_Value.java

--- InvertedIndex/File_Value.java
+++ InvertedIndex/File_Value.java
@@ -5,27 +5,43 @@ import java.io.DataOutput;
 import java.io.IOException;
 import org.apache.hadoop.io.Writable;
 
-public class File_Value implements Writable{
-    String file;
-    Integer value;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 
-    public File_Value(String file, Integer value){
-        this.file=file;
-        this.value=value;
+public class File_Value implements Writable {
+
+    Text file;
+    IntWritable value;
+
+    public File_Value(String file, Integer value) {
+        this.file = new Text(file);
+        this.value = new IntWritable(value);
     }
 
-    public File_Value(){}
+    public File_Value() {
+        this.file = new Text();
+        this.value = new IntWritable();
+    }
 
-    public void readFields(DataInput in) throws IOException
-    {
-        this.file = in.readUTF();
-        this.value = in.readInt();
+    public String getFile() {
+       return file.toString();
+    }
+
+    public int getValue() {
+        return value.get();
     }
 
     @Override
-    public void write(DataOutput out) throws IOException
-    {
-        out.writeUTF(this.file);
-        out.writeInt(this.value);
+    public void readFields(DataInput in) throws IOException {
+        file.readFields(in);
+        value.readFields(in);
     }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        file.write(out);
+        value.write(out);
+    }
+
 }
